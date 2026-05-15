ab1fc5e1a5b32910d5fcddccd8e405fa7be229bc
Keynan Santos <estudantekeynan@gmail.com>
2026-05-15 11:11:32 -0300

    novo projeto

---
+++ Python Keynan/projetos/projeto_brigada.py
@@ -0,0 +1,13 @@
+# #Contexto
+# #O Serviço Especializado em Engenharia de Segurança e em Medicina do Trabalho
+# (SESMT) precisa automatizar o controle de treinamentos obrigatórios (como CIPA,
+# Brigada de Incêndio e NR-35) e a entrega de Equipamentos de Proteção Individual (EPIs).
+# Objetivo
+# Desenvolva um programa em Python que gerencie o status de conformidade dos
+# funcionários de uma empresa.
+
+#RF: Armazenamento de dados, verificação de EPI, e liste os EPI Se o treinamento tiver mais de 2 anos, exiba a mensagem: "Treinamento Vencido! Encaminhar para reciclagem.Caso contrário, exiba: "Treinamento Válido Exiba na tela um resumo com o total de funcionários cadastrados e quantos
+#estão com treinamentos em dia."
+
+#RFN: Criar uma função que receba o ano do último treinamento da Brigada de
+#Incêndio.
